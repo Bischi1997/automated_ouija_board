0cbcfb8b01ab6c3b5f250e81a06b6333292c6b64
PiagetEPP <101410832+PiagetEPP@users.noreply.github.com>
2022-03-13 19:35:00 +0100

    Add Audioplayer, Add Wifi
    Wifi und HTML geht noch nicht. Nur <ESP8266WiFi> und Statische anzeige in HTML über Hotspot geht.
    Evtl. ESP als Access Point verwenden? (https://randomnerdtutorials.com/esp8266-nodemcu-access-point-ap-web-server/)

--- automated_ouija_board.ino
+++ automated_ouija_board.ino
@@ -1,21 +1,162 @@
+// Import required libraries
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
 #include <Servo.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
-
-Servo servo_base;   //give a name to the servo
+//give a name to the servo
+Servo servo_base;   
 Servo servo_arm;
 
-void setup() 
-{
+//Softwareserial für Audio Player
+SoftwareSerial mySoftwareSerial(D2, D1); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
+// Daten des WiFi-Netzwerks
+const char* ssid     = "Simon";
+const char* password = "qwe12345";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+
+
+void setup(){
+
+  //Servos Anbinden
   Serial.begin(115200);
   servo_base.attach(D0);
   servo_arm.attach(D3);
+
+  //Verbindung zu Audioplayer prüfen
+  mySoftwareSerial.begin(9600);
+  
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  };
+  Serial.println(F("DFPlayer Mini online."));
+
+  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
+  myDFPlayer.loop(1);  //Play the first mp3
+
+
+
+  // Initialize SPIFFS
+  //https://randomnerdtutorials.com/install-esp8266-filesystem-uploader-arduino-ide/
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  // Connect to Wi-Fi
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Lokale IP-Adresse im Seriellen Monitor ausgeben und Server starten
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+
+  // Route for root / web page
+  //https://randomnerdtutorials.com/esp8266-web-server-spiffs-nodemcu/
+  //https://randomnerdtutorials.com/esp8266-nodemcu-async-web-server-espasyncwebserver-library/
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false);
+  });
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  // Start server
+  server.begin();
+
 }
+
+//Meldungen von Audioplayer ausgeben
+void printDetail(uint8_t type, int value){
+switch (type) {
+  case TimeOut:
+    Serial.println(F("Time Out!"));
+    break;
+  case WrongStack:
+    Serial.println(F("Stack Wrong!"));
+    break;
+  case DFPlayerCardInserted:
+    Serial.println(F("Card Inserted!"));
+    break;
+  case DFPlayerCardRemoved:
+    Serial.println(F("Card Removed!"));
+    break;
+  case DFPlayerCardOnline:
+    Serial.println(F("Card Online!"));
+    break;
+  case DFPlayerPlayFinished:
+    Serial.print(F("Number:"));
+    Serial.print(value);
+    Serial.println(F(" Play Finished!"));
+    break;
+  case DFPlayerError:
+    Serial.print(F("DFPlayerError:"));
+    switch (value) {
+      case Busy:
+        Serial.println(F("Card not found"));
+        break;
+      case Sleeping:
+        Serial.println(F("Sleeping"));
+        break;
+      case SerialWrongStack:
+        Serial.println(F("Get Wrong Stack"));
+        break;
+      case CheckSumNotMatch:
+        Serial.println(F("Check Sum Not Match"));
+        break;
+      case FileIndexOut:
+        Serial.println(F("File Index Out of Bound"));
+        break;
+      case FileMismatch:
+        Serial.println(F("Cannot Find File"));
+        break;
+      case Advertise:
+        Serial.println(F("In Advertise"));
+        break;
+      default:
+        break;
+    }
+    break;
+  default:
+    break;
+}
+}
+
+
 
 void loop() 
 {
+
+  if (myDFPlayer.available()) {
+      printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+    };
+
+
+
+
   struct PositionStruct {
     String character;
     float x;
@@ -120,6 +261,8 @@ int calc_servo2(double x, double y, double arm1length, double arm2length){
 
 double calc_servo_1_angle (double input_angle) {
   int result;
-  result = map(input_angle, -90, 90, 0, 180);
+  result = map(input_angle, 0, 180, 180, 0);
   return result;
 }
+
+
